d2e57898cbb1a124aa8a5c75db9a171faaca7942
Ben Griffiths <mrbengriffiths@googlemail.com>
2017-12-05 14:05:31 +0000

    Update web.clj

--- src/clojure_rest_example/web.clj
+++ src/clojure_rest_example/web.clj
@@ -24,6 +24,7 @@
   []
   (let [body {:name "clojure-rest-example"
               :version version
+              :hello "world!"
               :success true
               :dependencies []}]
     {:headers {"content-type" "application/json"}
